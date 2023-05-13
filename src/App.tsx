50eaadb22c6f842a7aa15b6b792a06ba8ca2fd0b
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-05-13 14:11:32 -0300

    Simple productive list starter shape

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Router } from "./components/Router"
+import {BrowserRouter} from 'react-router-dom'
+
+function App() {
+
+  return (    
+    <BrowserRouter>
+      <Router/>
+    </BrowserRouter>
+  )
+}
+
+export default App
